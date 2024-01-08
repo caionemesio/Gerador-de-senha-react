76859c90899c7efb10df5c496d7df8563f4c5c48
Caio Nemesio <caionemesio2@gmail.com>
2024-01-08 15:19:36 -0300

    Otimizando código

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,46 @@
-import { useState } from 'react'
-import './App.css'
-import Title from './components/Title'
-import {GeneratePassword}  from 'js-generate-password'
-import CopyCplipboard from 'copy-to-clipboard'
+import { useState } from 'react';
+import './App.css';
+import Title from './components/Title';
+import { GeneratePassword } from 'js-generate-password';
+import CopyCplipboard from 'copy-to-clipboard';
 
+export default function App() {
 
+  const [password, setPassword] = useState("");
+  const [copy, setCopy] = useState("copiar");
+  const [buttonClass, setButtonClass] = useState('');
 
-function App() {
-
-  const [password, setPassword]= useState("")
-  const [copy, setCopy]=useState("copiar")
-  const [buttonClass, setButtonClass]= useState('')
-
-  function generateNewPassword(){
+  function generateNewPassword() {
     let newPassword = GeneratePassword({
-      length:12,
-      symbols:true,
-      numbers:true
-    })
-    setCopy('Copiar')
-    setButtonClass('buttonDefault')
-    return newPassword
+      length: 12,
+      symbols: true,
+      numbers: true
+    });
+    setCopy('Copiar');
+    setButtonClass('buttonDefault');
+    setPassword(newPassword)
+    
   }
 
-  function copyToTransferArea(password){
-    CopyCplipboard(password)
-    setCopy("Copiado")
-    setButtonClass('copped')
+  function copyToTransferArea() {
+    CopyCplipboard(password);
+    setCopy("Copiado");
+    setButtonClass('copped');
   }
+
   return (
     <>
       <Title>Gerador de senhas</Title>
       <div className="card">
-        <button onClick={()=>setPassword(generateNewPassword()) }>
+        <button onClick={generateNewPassword}>
           Gerar
-        </button >
+        </button>
 
-        <button className={buttonClass} onClick={()=>copyToTransferArea(password)}>
+        <button className={buttonClass} onClick={copyToTransferArea}>
           {copy}
         </button>
       </div>
       <h3>{password}</h3>
     </>
-  )
+  );
 }
-
-export default App
